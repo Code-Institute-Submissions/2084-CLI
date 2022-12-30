d29b224480ae657668c84b9d9bfc75e2cf88f294
Dylan <dylanbradley1@hotmail.com>
2022-12-30 17:32:33 +0000

    create 'grid' to print to console

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+array = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]
+
+for row in array:
+    print(row[0], row[1], row[2], row[3])
+
