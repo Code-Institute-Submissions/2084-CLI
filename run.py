ed595ebcae17827424d77875a75692de16a84c8c
Dylan <dylanbradley1@hotmail.com>
2022-12-30 18:35:35 +0000

    Created function which returns list containing [row, index] of any 'free cell'

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 array = [
     [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [2, 2, 2, 0],
+    [0, 3, 3, 0],
+    [4, 4, 0, 0],
 ]
 
 for row in array:
@@ -10,3 +10,17 @@ for row in array:
 
 def is_cell_free(grid, row, cell):
     return grid[row][cell] == 0
+
+def get_free_cells(grid):
+    free_cells = []
+    row_count = 0
+    
+    for row in grid:
+        row_count = row_count + 1
+        index_count = 0
+        for value_index in row:
+            index_count = index_count + 1
+            if value_index == 0:
+                free_cells.append([row_count, index_count])
+    
+    return(free_cells)
